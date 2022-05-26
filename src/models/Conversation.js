6b377867fc78c3c094cb0ad2e4d39b2c11233e02
Emilia Hansson <eh222yn@student.lnu.se>
2022-05-26 15:41:11 +0200

    Implement Conversation and Message model

---
+++ src/models/Conversation.js
@@ -0,0 +1,36 @@
+/**
+ * Mongoose model Conversation.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  members: {
+    type: Array
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    /**
+     * Removes sensitive information by transforming the resulting object.
+     *
+     * @param {object} doc The mongoose document to be converted.
+     * @param {object} ret The plain object response which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+      delete ret.__v
+    }
+  },
+  virtuals: true
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Creates a model using the schema.
+export const Conversation = mongoose.model('Conversation', schema)
