2ca0c11d58eddba016d3aa2372732c1522291d11
Emilia Hansson <eh222yn@student.lnu.se>
2022-05-26 16:44:05 +0200

    Add methods for creating a conversation and finding user conversations

---
+++ src/controllers/api/messages-controller.js
@@ -0,0 +1,16 @@
+/**
+ * Module for the ConversationsController.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.0.0
+*/
+
+import { Message } from '../../models/message.js'
+// import createError from 'http-errors'
+
+/**
+ * Encapsulates a controller.
+ */
+export class MessagesController {
+  
+}
--- src/routes/api/v1/conversations-router.js
+++ src/routes/api/v1/conversations-router.js
@@ -6,8 +6,14 @@
  */
 
 import express from 'express'
-import { ConversationsController } from '../../../controllers/api/messages-controller.js'
+import { ConversationsController } from '../../../controllers/api/conversations-controller.js'
 
 export const router = express.Router()
 
 const controller = new ConversationsController()
+
+router.param('id', (req, res, next, id) => controller.loadConversations(req, res, next, id))
+
+router.post('/', (req, res, next) => controller.create(req, res, next))
+
+router.get('/:id', (req, res, next) => controller.find(req, res, next))
